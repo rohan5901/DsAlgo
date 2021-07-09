88b6be77edeba899d09ea0c51e0e13826a22678a
rohan5901 <rohangarg5901@gmail.com>
2021-07-09 06:09:25 +0530

    Reverse using string

---
+++ reverseAstring.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void reverse(string s)
+{
+    if (s.length() == 0)
+    {
+        return;
+    }
+    string ros = s.substr(1);
+    reverse(ros);
+    cout << s[0] << " ";
+}
+
+int main()
+{
+    string s = "rohan";
+    reverse("rohan");
+}
