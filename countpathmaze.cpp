f8530dc526602e40e6a003c8864a7cca5d38d4ee
rohan5901 <rohangarg5901@gmail.com>
2021-07-12 23:14:44 +0530

    using recursion

---
+++ countpathmaze.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int countpathmaze(int n, int i, int j)
+{
+    if (i == n - 1 && j == n - 1)
+    {
+        return 1;
+    }
+
+    if (i >= n || j >= n)
+    {
+        return 0;
+    }
+
+    return (countpathmaze(n, i + 1, j) + countpathmaze(n, i, j + 1));
+}
+
+int main()
+{
+    cout << countpathmaze(3, 0, 0) << endl;
+
+    return 0;
+}
